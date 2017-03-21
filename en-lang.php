18d0513b59949ece776fe953f73241f9966ee28a
Мирослав Здравковић <miroslav.zdravkovic@gmail.com>
2017-03-21 15:52:34 +0100

    Added a new file - en-lang.php with english words

---
+++ en-lang.php
@@ -0,0 +1,90 @@
+<?php
+
+$company = 'Construction Project ltd';
+
+$title = 'Construction Project - constructing, designing, building';
+
+$language = ['English', 'Български', 'Deutsch', 'Српски'];
+
+$slogan = "consulting - designing - constructing - building";
+
+$menu = ['home', 'about us', 'services', 'portfolio', 'contact'];
+
+$slides = [
+    ['Welcome to ', 'Construction project ltd.'],
+    ['Look At ', 'Who We Are'],
+    ['Look At ', 'What We Do'],
+    ['Our ', 'Reference List'],
+    ['Contact ', 'Us']
+];
+
+$services_title = 'Our Services';
+
+$services_text = 'Below is the list of our services:';
+
+$services = [
+    ['Projects', '&nbsp;'],
+    ['Urban projects to build', '&nbsp;'],
+    ['Preparation of detailed regulation plans', '&nbsp;'],
+    ['Works and designing residential and commercial buildings', 'commercial, warehouse, manufacturing'],
+    ['Others design', 'auxiliary buildings, agricultural buildings'],
+    ['Studies on the assessment of environmental impact', '&nbsp;'],
+    ['Projects for legalization of buildings', '&nbsp;'],
+    ['Material specifications', 'and the estimated value'],
+    ['Projects of fire protection', '&nbsp;'],
+    ['Studies of energy efficiency', '&nbsp;'],
+    ['Electrical works for strong and weak currents', '&nbsp;'],
+    ['Locksmith and mechanical works', '&nbsp;']
+];
+
+$aboutus_title = 'About us';
+
+$aboutus_text_1 = '"Construction project" ltd. eood is a private company with a long tradition in the design and construction of facilities. Investors have helped to successfully start your business or apartment in a new location.';
+
+$aboutus_text_2 = '"Construction project" ltd. for you can do all by system - key in hands, urban design, preliminary and final design and all types of work. We have professional team of licensed construction engineer as well as electrical engineers and technicians who lead the firm and control the quailty of work.';
+
+$history_title = 'History';
+
+$history_text_1 = '"Construction project ltd." was founded in 1989. The beginnings are tied to an independent building, which two years later grew into a company for design and engineering "Construction project ltd.". Founder is Krumov Slobodan, graduated engineer of construction. In the following years, "Construction project" slowly but surely becoming one of the leaders of the works in the environment. In 1999. "Construction project ltd." received its final form of organization and became a limited liability company. We have built a long tradition on the territory of the former Yugoslavia, Russia, Germany.';
+
+$history_text_2 = 'Today, "Construction project ltd." is synonymous with professionalism, honesty, safety and quality. In the past 25 years we have gained knowledge and experience that today "Constrution project ltd" can do all for you by system - key in hands. From procurement of land, urban design, conceptual design, preliminary and detailed design, to all types of work.';
+
+$portfolio_title = 'Our Portfolio';
+
+$portfolio_text = 'This is just a part of our works to which we are very proud.';
+
+$portfolio = [
+    ['Rough construction works', 'Doing any kind of contruction works and preparations'],
+    ['Facade', 'Facade decoration'],
+    ['Gypsum', 'Plaster works'],
+    ['Klinka', 'Lining the walls of brick'],
+    ['Painting', 'Painting walls and facades'],
+    ['Underfloor cladding', 'Coating all types of floors and surfaces'],
+    ['Interior design', 'Coating all types of floors and surfaces'],
+    ['Exterior design', 'Coating all types of floors and surfaces']
+];
+
+$contact_title = 'Contact Us';
+
+$contact_text_1 = 'Complete and sumbit the form below to send an email to us.
+                  You`ll get an answer as soon as possible. Fields marked with <span class="required">* </span> are required.';
+
+$contact_text_2 = 'This is the place where you can contact us. Feel free to ask any questions concerning our job and you`ll get an answer as soon as possible.';
+
+$error_name = 'You must fill out your name and it must be without numbers.';
+
+$error_email = 'You must fill out a valid email address.';
+
+$error_subject = 'You must fill out the subject for your message.';
+
+$error_message = 'Message can`t be shorter than 10 chars nor longer than 300 chars and can contain only letters, numbers and sign of interpuctions.';
+
+$company_name = 'Company';
+
+$company_address = 'Address';
+
+$company_phone = 'Phone';
+
+$company_email = 'Email';
+
+$company_website = 'Website';
